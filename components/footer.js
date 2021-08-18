936cc05c59ebb54ee486f0c65e60993e27bac719
Sam Bluestone <bluestones22@mail.wlu.edu>
2021-08-18 15:29:41 -0400

    change class to className

--- components/footer.js
+++ components/footer.js
@@ -1,7 +1,7 @@
 export default function Footer() {
     return (
       <footer>
-          <span class="text-muted">Open source project created by Sam Bluestone  <a href="https://github.com/sambluestone/personal-website"><img src="github.png" alt="GitHub logo" style={{width: "20px", height: "20px"}}/></a></span>
+          <span className="text-muted">Open source project created by Sam Bluestone  <a href="https://github.com/sambluestone/personal-website"><img src="github.png" alt="GitHub logo" style={{width: "20px", height: "20px"}}/></a></span>
       </footer>
     );
 }
